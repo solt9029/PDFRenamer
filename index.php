1d45c98abc9452c6eda5e3cb6525d5aa6522f8cc
Kenshi Shiode <atemu1212@yahoo.co.jp>
2017-05-13 18:49:52 +0900

    ファイル名変更ファンクション追加

--- index.php
+++ index.php
@@ -1,12 +1,16 @@
 <?php
 
+error_reporting(E_ALL);
+
 include "vendor/autoload.php";
 
-$parser=new \Smalot\PdfParser\Parser();
-$pdf=$parser->parseFile("document.pdf");
-$details=$pdf->getDetails();
-$title=$details["Title"];
-
-var_dump($details);
-
-echo $title;
+//一つ上の階層のPDFファイルをすべて取得する
+foreach(glob("../*.pdf") as $filename){
+	$parser=new \Smalot\PdfParser\Parser();
+	$pdf=$parser->parseFile($filename);
+	$details=$pdf->getDetails();
+	$title=$details["Title"];
+	//ファイル名として使用できない文字列を置換(windows)
+	$title=str_replace(array(":","/","\n","?","*","<",">","|",'"')," ",$title);
+	rename($filename,"../".$title.".pdf");
+}
